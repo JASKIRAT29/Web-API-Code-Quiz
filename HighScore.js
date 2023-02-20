6d1a376a33bb355fe2e388974650bf795e25dd8b
JASKIRAT29 <jaskiratkr29@gmail.com>
2023-02-20 12:40:19 -0500

    changes code

--- HighScore.js
+++ HighScore.js
@@ -1,27 +1,27 @@
 // Var with array and object for questions 
 var questions = [
     {
-        title: "Commonly used data types DO NOT include:",
+        title: "Q1. Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
         answer: "alerts"
     },
     {
-        title: "The condition in an if / else statement is enclosed within ____.",
+        title: "Q2. The condition in an if / else statement is enclosed within ____.",
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
         answer: "parentheses"
     },
     {
-        title: "Arrays in Javascript can be used to store ____.",
+        title: "Q3. Arrays in Javascript can be used to store ____.",
         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
         answer: "all of the above"
     },
     {
-        title: "String values must be enclosed within ____ when being assigned to variables.",
+        title: "Q4. String values must be enclosed within ____ when being assigned to variables.",
         choices: ["commas", "curly brackets", "quotes", "parenthesis"],
         answer: "quotes"
     },
     {
-        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        title: "Q5. A very useful tool for used during development and debugging for printing content to the debugger is:",
         choices: ["Javascript", "terminal / bash", "for loops", "console log"],
         answer: "console log"
     },
@@ -78,19 +78,7 @@ function render(questionIndex) {
         var userQuestion = questions[questionIndex].title;
         var userChoices = questions[questionIndex].choices;
         questionsDiv.textContent = userQuestion;
-        var name = "radio"+i; 
-        for ( var opt in options ) {
         
-          var radioEle = document.createElement("input");
-          radioEle.type = "radio";          
-          radioEle.value = options[opt];
-          radioEle.name = name;
-          document.body.appendChild(radioEle);
-          var label = document.createElement("Label");
-          label.innerHTML = options[opt];
-          document.body.appendChild(label);
-          document.body.appendChild(document.createElement("br"));
-        }
     }
     // New for each for question choices
     userChoices.forEach(function (newItem) {
